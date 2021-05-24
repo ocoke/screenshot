5d24eb1004b5d46b689b9cd8c0d12b2425e2b39c
YFun <omycoke@outlook.com>
2021-05-24 18:42:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ file = open("./list.txt")
 
 for line in file.readlines():
     domain = line.strip('\n')
-    url = 'http://image.thum.io/get/width/1024/crop/768/allowJPG/noanimate/wait/5/https://{}'.format(domain)
+    url = 'http://image.thum.io/get/width/1024/crop/768/allowJPG/noanimate/wait/5/maxAge/12/https://{}'.format(domain)
     print(domain)
     try:
         res =  req.get(url)
-        with open('./public/{}.jpg'.format(domain), "wb") as file:
+        with open('public/{}.jpg'.format(domain), "wb") as file:
             file.write(res.content)
     except:
         print("Error.")
