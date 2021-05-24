fc56524aea37947d1ba80824ade0911340dc6691
oCoke <oCoke@users.noreply.github.com>
2021-05-24 10:37:12 +0000

    deploy: 204fabf6e3eb0ce72fdf25977b936c31a73b5a66

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests as req
+
+file = open("./list.txt")
+
+for line in file.readlines():
+    domain = line.strip('\n')
+    url = 'http://image.thum.io/get/width/1024/crop/768/allowJPG/noanimate/wait/5/https://{}'.format(domain)
+    print(domain)
+    try:
+        res =  req.get(url)
+        with open('{}.jpg'.format(domain), "wb") as file:
+            file.write(res.content)
+    except:
+        print("Error.")
+    
+
+file.close()
